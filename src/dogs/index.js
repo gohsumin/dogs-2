c721cd1cda2a2931d3fc5fc90574e710fb7908c6
suminnie <sumin.goh@peachpay.app>
2023-07-14 19:42:58 -0400

    getting all images in the slideshow

--- src/dogs/index.js
+++ src/dogs/index.js
@@ -8,22 +8,80 @@ async function getDogs() {
 	}
 }
 
+async function getDogImages(mainBreed, subBreed = null, n = 8) {
+	const response = await fetch(`https://dog.ceo/api/breed/${mainBreed}${subBreed ? `/${subBreed}` : ''}/images/random/${n}`);
+	const json = await response.json();
+	if (json.status === 'success') {
+		return json.message;
+	} else {
+		throw Error();
+	}
+}
+
+const cards = {};
+
+function makeSlideshow(mainBreed) {
+	const $slideshow = document.createElement('div');
+	$slideshow.className = `slideshow ${mainBreed}`;
+	const images = cards[mainBreed].images;
+	if (!Array.isArray(images)) {
+		Object.getOwnPropertyNames(images).forEach(subBreed => {
+			images[subBreed].forEach(img => {
+				const $slide = document.createElement('div');
+				$slide.className = `slide ${subBreed}`;
+				const $img = document.createElement('img');
+				$img.src = img;
+				$slide.append($img);
+				$slideshow.append($slide);
+			});
+		});
+	} else if (Array.isArray(images)) {
+		images.forEach(img => {
+			const $slide = document.createElement('div');
+			$slide.className = 'slide';
+			const $img = document.createElement('img');
+			$img.src = img;
+			$slide.append($img);
+			$slideshow.append($slide);
+		});
+	}
+	return $slideshow;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	getDogs().then(dogs => {
 		const $ul = document.createElement('ul');
-		Object.keys(dogs).forEach(mainBreed => {
+		Object.keys(dogs).forEach(async mainBreed => {
 			const $li = document.createElement('li');
-			$li.innerHTML = mainBreed;
+			const $name = document.createElement('div');
+			$name.className = 'dog-name';
+			$name.innerHTML = mainBreed;
+			$li.append($name);
+			const minNumImages = 20;
+
 			if (dogs[mainBreed].length) {
 				const $innerUl = document.createElement('ul');
-				dogs[mainBreed].forEach(subBreed => {
+				const numSubBreedImages = (minNumImages / dogs[mainBreed].length < 1)
+					? (dogs[mainBreed].length * 2)
+					: (minNumImages / dogs[mainBreed].length);
+				let subBreedImages = {};
+				for (subBreed of dogs[mainBreed]) {
 					const $innerLi = document.createElement('li');
 					$innerLi.innerHTML = subBreed;
 					$innerUl.append($innerLi);
-				})
+					subBreedImages[subBreed] = await getDogImages(mainBreed, subBreed, numSubBreedImages);
+				}
 				$li.append($innerUl);
+
+				cards[mainBreed] = { images: subBreedImages };
+			} else {
+				cards[mainBreed] = { images: await getDogImages(mainBreed, null, minNumImages) };
 			}
+
+			const $slideshow = makeSlideshow(mainBreed);
+			$li.append($slideshow);
 			$ul.append($li);
+
 		});
 		document.querySelector('main').append($ul);
 	});
